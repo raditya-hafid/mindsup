b03b8cb39d46d0c422399268f2d6919d2ab9451a
raditya-hafid <radityahafid123@gmail.com>
2025-04-15 09:30:03 +0700

    hapus

--- head/head.php
+++ head/head.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/nav.css">
+    <!-- <link rel="stylesheet" href="../css/nav.css"> -->
     <link rel="stylesheet" href="../css/simulasi.css">
     
 </head>
