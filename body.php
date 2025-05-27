c7df7d80a24e939831f73a9c074711010d1477cf
Muhammad Iqbal <muhammadiqbal21675@gmail.com>
2025-05-27 13:10:48 +0700

    Update body.php
    edit about kalo di klik dia pas ke about ga ketinggian atau kerendahan

--- body.php
+++ body.php
@@ -69,113 +69,6 @@
       <p>Buka kapan saja, di mana saja.</p>
     </div>
   </div>
-
-<<<<<<< HEAD
-            <!-- Featured Courses Section 1 -->
-             <div>
-                <div class="container-fluid featured-courses">
-
-
-                    <section id="whychooseus">
-                        <div class="contaier">
-                          <div id="pembungkus" class="row">
-                            <div id="gambar-kiri" class="col-md-5">
-                              <img class="img-fluid h-100 w-100 object-fit-cover" src="../asset/foto.jpeg" alt="foto">
-                            </div>
-                            <div id="misi-contents" class="col-md-7">
-                              <div class="row">
-                                <h1 class="mb-4">Kenapa Memilih Kami</h1>
-                                <h3>Lihat Apa Misi Kami</h3>
-                              </div>
-                              <div id="misi-content-1" class="row">
-                                <div id="misi-content-1-number" class="col-1">01</div>
-                                <div id="misi-content-1-explain" class="col-11">
-                                  <div id="misi-content-1-explain-title" class="row h5">Membantu Anak Belajar Lebih Mudah</div>
-                                  <div id="misi-content-1-explain-again" class="row">Minds Up ingin membuat anak-anak lebih mudah
-                                    memahami pelajaran dengan cara yang simpel dan tidak membingungkan.
-                                    Materinya dibuat ringan dan jelas supaya anak tidak merasa sulit saat belajar.
-                                  </div>
-                                </div>
-                              </div><br>
-                              <div id="misi-content-2" class="row">
-                                <div id="misi-content-2-number" class="col-1">02</div>
-                                <div id="misi-content-2-explain" class="col-11">
-                                  <div id="misi-content-2-explain-title" class="row h5">Belajar dengan Cara yang Menyenangkan</div>
-                                  <div id="misi-content-2-explain-again" class="row">Supaya anak tidak cepat bosan, Minds Up berusaha
-                                    membuat pembelajaran lebih seru, misalnya dengan gambar menarik, video menarik, atau latihan yang
-                                    tidak terlalu berat.
-                                  </div>
-                                </div>
-                              </div><br>
-                              <div id="misi-content-3" class="row">
-                                <div id="misi-content-3-number" class="col-1">03</div>
-                                <div id="misi-content-3-explain" class="col-11">
-                                  <div id="misi-content-3-explain-title" class="row h5">Membantu Anak Lebih Percaya Diri</div>
-                                  <div id="misi-content-3-explain-again" class="row">Minds Up ingin membuat anak lebih percaya diri saat
-                                    belajar. Materi disusun agar mudah dimengerti, dan ada latihan
-                                    ringan supaya mereka bisa mencoba sendiri tanpa takut salah.</div>
-                                </div>
-                              </div><br>
-                              <div id="misi-content-4" class="row">
-                                <div id="misi-content-4-number" class="col-1">04</div>
-                                <div id="misi-content-4-explain" class="col-11">
-                                  <div id="misi-content-4-explain-title" class="row h5">Menyediakan Materi yang Mudah Dipahami</div>
-                                  <div id="misi-content-4-explain-again" class="row">Materi di Minds Up dibuat dengan bahasa yang
-                                    sederhana dan contoh yang jelas. Anak-anak bisa membaca dan belajar tanpa
-                                    perlu merasa kesulitan memahami isi pelajaran.</div>
-                                  </div>
-                                </div><br>
-                            </div>
-                          </div>
-                        </div>
-                      </section>
-                </div>
-             </div>
-            
-            <!-- Featured Courses Section 2 -->
-            <div class="container-fluid featured-courses">
-                <div style="text-align: center;" class="row justify-content-md-center">
-                    <div class="col-md-9">
-                        <h1>POPULAR COURSES</h1>
-                        <hr>
-                        <p>
-                        Raih Prestasi dengan Kelas Favorit <br/>Temukan program unggulan kami yang dirancang untuk membuat belajar menyenangkan dan efektif!
-                        </p>
-                    </div>
-                </div>
-
-                <?php
-                  require '../komponen/koneksi.php';
-
-                  $sql = "SELECT `gambar`, `judul`, `deskripsi` FROM `kursus` ORDER BY `id_kursus` DESC LIMIT 3";
-                  $result = mysqli_query($conn, $sql);
-
-                  $dataCourse = [];
-                  if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                      $dataCourse[] = $row;
-                    }
-                  } else {
-                    echo "Tidak ada data";
-                  }
-                ?>
-
-                <div class="row justify-content-md-center">
-                  <?php
-                    $index = 0;
-                    while ($index < count($dataCourse)) {
-                      echo '<div class="col-md-3 rounded panel-course">';
-                      echo '<img src="../input page/' . $dataCourse[$index]['gambar'] . '" width="100%" height="auto" alt="Cover Image">';
-                      echo '<h5 style="font-size: 22px; margin-top: 12px;">' . $dataCourse[$index]['judul'] . '</h5>';
-                      echo '<p style="text-align: justify; word-wrap: break-word;">' . $dataCourse[$index]['deskripsi'] . '</p>';
-                      echo '<button class="btn btn-dark">Read More</button>';
-                      echo '</div>';
-
-                      $index = $index + 1;
-                    }
-                  ?>
-                </div>
-=======
   <!-- Featured Courses Section 1 -->
   <div>
     <div class="container-fluid featured-courses">
@@ -184,7 +77,6 @@
           <div id="pembungkus" class="row">
             <div id="gambar-kiri" class="col-md-6 mb-4 mb-md-0 d-flex">
               <img class="img-fluid rounded shadow-sm" src="../asset/foto.jpeg" alt="Kenapa Memilih Kami" style="object-fit: cover; width: 100%; height: 100%;">
->>>>>>> parent of 4f3dec8 (sistem menambahkan course)
             </div>
             <div id="misi-contents" class="col-md-6 d-flex flex-column justify-content-center">
               <div class="ps-md-4">
